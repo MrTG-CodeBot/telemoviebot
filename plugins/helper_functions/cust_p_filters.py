5fcf9b65f4bc25ab794b062e0e8f7e9c6ff234c0
Amal Nath <134857584+kyojuro6engoku@users.noreply.github.com>
2023-10-07 22:06:37 +0530

    Update cust_p_filters.py

--- plugins/helper_functions/cust_p_filters.py
+++ plugins/helper_functions/cust_p_filters.py
@@ -1,9 +1,3 @@
-
-
-
-
-
-
 <!DOCTYPE html>
 <html lang="en" data-color-mode="auto" data-light-theme="light" data-dark-theme="dark"  data-a11y-animated-images="system" data-a11y-link-underlines="false">
 
