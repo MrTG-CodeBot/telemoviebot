1a38cc4738ca62c5808b0a859e1e446a0098ca1f
Amal Nath <134857584+kyojuro6engoku@users.noreply.github.com>
2023-10-07 22:00:10 +0530

    Delete plugins/helpers directory

--- plugins/helpers/mod/clone.py
+++
@@ -1,7 +0,0 @@
-from pyrogram import Client
-from pyrogram.types import Message
-
-async def cloner_handler(client: Client, message: Message):
-    await message.reply("This is the cloner command handler.")
-
-# You can define other handlers specific to the cloner command if needed.
--- plugins/helpers/mod/start.py
+++
@@ -1,7 +0,0 @@
-from pyrogram import Client
-from pyrogram.types import Message
-
-async def start_handler(client: Client, message: Message):
-    await message.reply("Welcome! This is the start command handler.")
-
-# You can define other handlers specific to the start command if needed.
