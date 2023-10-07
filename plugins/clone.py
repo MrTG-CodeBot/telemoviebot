cf08e92f15527259a873161a89a95f4e8560f5b1
Amal Nath <134857584+kyojuro6engoku@users.noreply.github.com>
2023-10-07 22:21:29 +0530

    Update clone.py

--- plugins/clone.py
+++ plugins/clone.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 
-@app.on_message(filters.command("clone") & filters.private)
+@Client.on_message(filters.command("clone") & filters.private)
 async def clone(client, message):
     if len(message.command) < 2:
         await message.reply_text("<b>Adding soon or not</b>.")
