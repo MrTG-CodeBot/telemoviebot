b7ae6ef2e44fd90881ba98c000b5f359ab6d1cf1
Amal Nath <134857584+kyojuro6engoku@users.noreply.github.com>
2023-10-14 13:06:30 +0530

    Update Insta.py

--- plugins/helper_functions/Insta.py
+++ plugins/helper_functions/Insta.py
@@ -1,48 +1,56 @@
 from pyrogram import Client, filters
-import requests
 import os
-from info import API_ID,API_ID,BOT_TOKEN
-from datetime import date, datetime 
+import time
+import requests  # Add this import
 
-# Create a new Pyrogram client
-Bot = Client("my_bot")
+app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
-# Define the command handler for the '/start' command
-@Client.on_message(filters.command("start"))
+@app.on_message(filters.command("start"))
 def start_command(client, message):
-    # Send a welcome message to the user
     message.reply_text("Welcome to the Instagram Reels Downloader Bot!")
 
-# Define the command handler for the '/download' command
-@Client.on_message(filters.command("download"))
+@app.on_message(filters.command("download"))
 def download_command(client, message):
-    # Get the Instagram Reels URL from the command arguments
     url = message.text.split(" ")[1]
 
     try:
-        # Send a message to inform the user that the download is in progress
         progress_message = message.reply_text("Downloading...")
 
-        # Get the video URL by making a request to Instagram
         response = requests.get(url)
         if response.status_code == 200:
-            # Create a filename based on the timestamp
             filename = f"reels_{int(time.time())}.mp4"
             
-            # Save the video content to a file
             with open(filename, "wb") as file:
                 file.write(response.content)
             
-            # Send the downloaded video to the user
             message.reply_document(document=filename)
             
-            # Clean up the temporary file
             os.remove(filename)
 
-            # Inform the user that the download is complete
             progress_message.edit_text("Download complete!")
         else:
             progress_message.edit_text("Error: Unable to download the Reels.")
+    except Exception as e:
+        progress_message.edit_text(f"An error occurred: {str(e)}")
+
+@app.on_message(filters.command("create"))
+def create_command(client, message):
+    try:
+        progress_message = message.reply_text("Creating Reels...")
+
+        # Add your video creation logic here using a library like MoviePy
+        # For example, you can use MoviePy to create a video and save it with a filename
+
+        # Replace the following line with your video creation logic
+        # filename = f"reels_{int(time.time())}.mp4"
+        
+        # Send the created video to the user
+        # message.reply_document(document=filename)
+        
+        # Clean up the temporary file
+        # os.remove(filename)
+
+        progress_message.edit_text("Reels creation complete!")
     except Exception as e:
         progress_message.edit_text(f"An error occurred: {str(e)}")
 
