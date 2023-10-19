0a231d4dd636e3ceb3fe5b438bbf53e117f02b2f
Sakurarendbot <148360334+Sakurarendbot@users.noreply.github.com>
2023-10-19 19:42:42 +0530

    Update and rename clone.py to check_ott.py

---
+++ plugins/check_ott.py
@@ -0,0 +1,35 @@
+import asyncio
+from pyrogram import Client, filters
+import requests
+from info import OMDB_API_KEY
+
+# Define a command to check OTT release
+@app.on_message(filters.command("checkott"))
+async def check_ott_release(_, message):
+    # Get the movie title from the user's command
+    if len(message.command) < 2:
+        await message.reply_text("Please provide the movie title with the command.")
+        return
+
+    movie_title = " ".join(message.command[1:])
+
+    # Send a request to the OMDb API to retrieve movie information
+    omdb_url = f"http://www.omdbapi.com/?t={movie_title}&apikey={omdb_api_key}"
+    response = requests.get(omdb_url)
+
+    if response.status_code == 200:
+        data = response.json()
+        if data.get("Response", "").lower() == "true":
+            title = data.get("Title", "N/A")
+            ott_platform = data.get("Website", "N/A")
+            if ott_platform == "N/A":
+                ott_status = "Not available on any OTT platform."
+            else:
+                ott_status = f"Available on {ott_platform}."
+
+            ott_release_message = f"{title}:\n{ott_status}"
+            await message.reply_text(ott_release_message)
+        else:
+            await message.reply_text("Movie not found. Please check the movie title.")
+    else:
+        await message.reply_text("An error occurred while fetching movie data. Please try again later.")
--- plugins/clone.py
+++
@@ -1,6 +0,0 @@
-from pyrogram import Client, filters
-
-@Client.on_message(filters.command("clone") & filters.private)
-async def clone(client, message):
-    if len(message.command) < 2:
-        await message.reply_text("<b>Adding soon or not</b>.")
