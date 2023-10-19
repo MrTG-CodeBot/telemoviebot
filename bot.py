7b2ae4d23734ac6a453d372c61db9fe3b0a0dc08
SakuraScripter <148360334+Sakurarendbot@users.noreply.github.com>
2023-10-19 22:05:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -55,6 +55,7 @@ class Bot(Client):
         now = datetime.now(tz)
         time = now.strftime("%H:%M:%S %p")
         await self.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
+        app = web.AppRunner(await web_server())
         await app.setup()
         bind_address = "0.0.0.0"
         await web.TCPSite(app, bind_address, PORT).start()
