3784577cef112e54d8a1ee6945962faf1bb3bb34
SakuraScripter <148360334+Sakurarendbot@users.noreply.github.com>
2023-10-20 19:41:27 +0530

    Create openai.py

---
+++ plugins/openai.py
@@ -0,0 +1,24 @@
+from pyrogram import Client, filters
+import openai
+from info import openai.api_key
+
+# Define a filter to handle the /ai command
+@Client.on_message(filters.command("ai", prefixes="/"))
+async def ai_command(_, message):
+    if len(message.command) < 2:
+        await message.reply_text("Please provide a question or input with the /ai command.")
+        return
+
+    user_input = " ".join(message.command[1:])
+    response = generate_response_with_gpt3(user_input)
+    await message.reply_text(response)
+
+# Function to generate a response using GPT-3
+def generate_response_with_gpt3(user_input):
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=user_input,
+        max_tokens=150,  # Adjust the response length as needed
+    )
+    return response.choices[0].text
+
