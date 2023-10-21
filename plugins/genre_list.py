42c614052216e53f3e1b9ff9372efee592abd675
SakuraScripter <148360334+Sakurarendbot@users.noreply.github.com>
2023-10-21 12:13:10 +0530

    Update genre_list.py

--- plugins/genre_list.py
+++ plugins/genre_list.py
@@ -1,1 +1,20 @@
+# List of Movie Genres
+movie_genres = [
+    "Action",
+    "Adventure",
+    "Animation",
+    "Comedy",
+    "Crime",
+    "Drama",
+    "Family",
+    "Fantasy",
+    "History",
+    "Horror",
+    "Mystery",
+    "Romance",
+    "Science Fiction",
+    "Thriller",
+    "War",
+    "Western",
+]
 
