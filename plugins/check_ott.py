57bfafe1e3963844c33cfe3a2d60e45c9ee4c060
SakuraScripter <148360334+Sakurarendbot@users.noreply.github.com>
2023-10-21 08:18:47 +0530

    Update check_ott.py

--- plugins/check_ott.py
+++ plugins/check_ott.py
@@ -1,48 +1,23 @@
-import asyncio
 from pyrogram import Client, filters
-import requests
-from info import OMDB_API_KEY
+from info import API_ID,API_HASH,BOT_TOKEN
 
-# Define a command to check OTT release
-@Client.on_message(filters.command("checkott"))
-async def check_ott_release(_, message):
-    # Get the movie title from the user's command
-    if len(message.command) < 2:
-        await message.reply_text("Please provide the movie title with the command.")
-        return
 
-    movie_title = " ".join(message.command[1:])
+@Client.on_message(filters.command("/check_ott"))
+async def search_movie(client, message):
+    # Extract the user's query
+    query = message.text.split(" ", 1)[1]
 
-    # Send a request to the OMDb API to retrieve movie information
-    omdb_url = f"http://www.omdbapi.com/?t={movie_title}&apikey=42d8ac83"
-    response = requests.get(omdb_url)
+    # Use your chosen movie/series database or API to retrieve information
+    # Replace this with your actual database/API request
+    result = search_movie_in_database(query)
 
-    if response.status_code == 200:
-        data = response.json()
-        if data.get("Response", "").lower() == "true":
-            title = data.get("Title", "N/A")
-            plot = data.get("Plot", "No plot available.")
-            released = data.get("Released", "N/A")
-            ratings = data.get("Ratings", [])
-            ott_platform = data.get("Website", "N/A")
-
-            ott_status = f"Title: {title}\nReleased: {released}\nPlot: {plot}"
-
-            if ott_platform == "N/A":
-                ott_status += "\nNot available on any OTT platform."
-            else:
-                ott_status += f"\nAvailable on {ott_platform}."
-
-            if ratings:
-                ott_status += f"\nRatings:"
-                for rating in ratings:
-                    source = rating.get("Source", "N/A")
-                    value = rating.get("Value", "N/A")
-                    ott_status += f"\n- {source}: {value}"
-
-            await message.reply_text(ott_status)
-        else:
-            await message.reply_text("Movie not found. Please check the movie title.")
+    if result:
+        release_date = result.get("release_date", "Release date not found")
+        ott_platform = result.get("ott_platform", "OTT platform not found")
+        response = f"Release Date: {release_date}\nOTT Platform: {ott_platform}"
     else:
-        await message.reply_text("An error occurred while fetching movie data. Please try again later.")
+        response = "Movie/series not found."
+
+    # Send the response to the user
+    await message.reply(response)
 
