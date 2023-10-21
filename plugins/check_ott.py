9d74f3e455f0df381278935ceab7bcbaaee2297a
SakuraScripter <148360334+Sakurarendbot@users.noreply.github.com>
2023-10-21 12:57:06 +0530

    Update check_ott.py

--- plugins/check_ott.py
+++ plugins/check_ott.py
@@ -2,26 +2,6 @@ from pyrogram import Client, filters
 from imdb import IMDb
 from info import API_ID, API_HASH, BOT_TOKEN
 
-# List of Movie Genres
-movie_genres = [
-    "Action",
-    "Adventure",
-    "Animation",
-    "Comedy",
-    "Crime",
-    "Drama",
-    "Family",
-    "Fantasy",
-    "History",
-    "Horror",
-    "Mystery",
-    "Romance",
-    "Science Fiction",
-    "Thriller",
-    "War",
-    "Western",
-]
-
 # Initialize IMDbPY
 ia = IMDb()
 
