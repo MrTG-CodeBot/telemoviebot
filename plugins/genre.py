507d6673c2c8e32b305d79347a575c6b3fde4df6
SakuraScripter <148360334+Sakurarendbot@users.noreply.github.com>
2023-10-21 17:56:42 +0530

    Update genre.py

--- plugins/genre.py
+++ plugins/genre.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 from imdb import IMDb
-from info import API_ID, API_HASH, BOT_TOKEN,TMDB_API_KEY
+from info import API_ID,API_HASH,BOT_TOKEN
 
 # Initialize IMDbPY
 ia = IMDb()
@@ -8,27 +8,17 @@ ia = IMDb()
 @Client.on_message(filters.command("search_genre"))
 async def search_genre(client, message):
     # Extract the user's query
-    user_input = message.text.split(" ", 1)[1]
+    genre = message.text.split(" ", 1)[1]
 
-    # Check if the user's input matches a valid genre
-    genre = None
-    if query in movie_genres:
-        if user_input.lower() in valid_genre.lower():
-            genre = valid_genre
-            break
+    # Search for movies by genre using IMDbPY
+    movies = ia.get_keyword(genre)
 
-    if genre:
-        # Search for movies by genre using IMDbPY
-        movies = ia.get_keyword(genre)
-
-        # Extract and format the results
-        if movies:
-            movie_list = "\n".join(movie["title"] for movie in movies)
-            response = f"Movies in the {genre} genre:\n{movie_list}"
-        else:
-            response = f"No movies found in the {genre} genre."
+    # Extract and format the results
+    if movies:
+        movie_list = "\n".join(movie["title"] for movie in movies)
+        response = f"Movies in the {genre} genre:\n{movie_list}"
     else:
-        response = "Invalid genre. Please choose from the following genres:\n" + "\n".join(movie_genres)
+        response = f"No movies found in the {genre} genre."
 
     # Send the response to the user
     await message.reply(response)
