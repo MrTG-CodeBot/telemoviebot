b4d6005778ba2af4b07a922236eea91da54282ab
SakuraScripter <148360334+Sakurarendbot@users.noreply.github.com>
2023-10-22 23:40:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -47,8 +47,6 @@ API_HASH = os.environ.get('API_HASH', '652bae601b07c928b811bdb310fdb4b0')
 BOT_TOKEN = os.environ.get('BOT_TOKEN', '6629383271:AAE1ZdxlW0ZMwbhGNXMdZpCvQZaW4LPDgX8')
 TMDB_API_KEY = os.environ.get('b0d58dcd0ccbe19340aa143daf4c6ad0')
 OMDB_API_KEY = os.environ.get('42d8ac83')
-OPENAI_ORGANIZATION = os.environ.get('org-1KWuVrdSyt25Vfvs8rvS3ofB')
-OPENAI_API_KEY = os.environ.get('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')
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
