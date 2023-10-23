7d53bda9ef01e93a056d68958c7692647fc31444
SakuraScripter <148360334+Sakurarendbot@users.noreply.github.com>
2023-10-23 13:18:43 +0530

    Update code_execution.py

--- plugins/code_execution.py
+++ plugins/code_execution.py
@@ -101,18 +101,3 @@ def bot_status(client, message):
     bot_info += f"**RAM Usage: {psutil.virtual_memory().percent}%**"
     message.reply_text(bot_info)
 
-# User Assistance command
-@Client.on_message(filters.command("helper") & filters.chat(AUTHORIZED_GROUPS))
-async def user_assistance(client, message: Message):
-    try:
-        if len(message.command) > 1:
-            issue = " ".join(message.command[1:])
-            assistance_request = f"User {message.from_user.id} requests assistance:\n{issue}"
-
-            await client.send_message(chat_id=admin_chat_id, text=assistance_request)
-            await message.reply_text("**Your assistance request has been sent to the admin team. They will get back to you.**")
-        else:
-            await message.reply_text("Usage: /helper [issue description]")
-    except Exception as e:
-        print(f"**An error occurred: {e}**")
-        await message.reply_text("**Sorry, something went wrong while processing your request. Please try again later.**")
