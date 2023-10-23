251c9832d36655a18c0ff5f79bd2f5cb9fb0bddd
SakuraScripter <148360334+Sakurarendbot@users.noreply.github.com>
2023-10-23 17:24:56 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -17,6 +17,7 @@ from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerId
 from utils import get_size, is_subscribed, get_poster, search_gagala, temp, get_settings, save_group_settings
 from database.users_chats_db import db
 from plugins import genre
+from plugins import uptime
 from database.ia_filterdb import Media, get_file_details, get_search_results
 from database.filters_mdb import (
     del_all,
