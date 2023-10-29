6d5a33f199855a3c7a3fe3d30bcf94ed62b7a288
SakuraScripter <148360334+Sakurarendbot@users.noreply.github.com>
2023-10-29 20:03:50 +0530

    Update ai.py

--- plugins/ai.py
+++ plugins/ai.py
@@ -2,7 +2,10 @@ import os
 import openai
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from info import API_ID,API_HASH, BOT_TOKEN, OPENAI_API_KEY
+from info import API_ID, API_HASH, BOT_TOKEN, OPENAI_API_KEY
+
+# Initialize a variable to keep track of the number of requests
+request_count = 0
 
 # Set your OpenAI API key
 openai.api_key = OPENAI_API_KEY
@@ -10,21 +13,38 @@ openai.api_key = OPENAI_API_KEY
 # Define a command for users to ask questions
 @Client.on_message(filters.command("ask", prefixes="/"))
 def ask_question_command(client: Client, message: Message):
-    # Extract the user's question from the command
-    user_question = message.text.split(" ", 1)[1]
+    global request_count  # Access the request_count variable
 
-    # Define a system message and user message
-    messages = [
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": user_question}
-    ]
+    try:
+        # Extract the user's question from the command
+        user_question = message.text.split(" ", 1)[1]
 
-    # Call the OpenAI API to get a response
-    response = openai.ChatCompletion.create(
-        model="gpt-4",  # Use "gpt-4" if you have early access, otherwise "gpt-3.5-turbo"
-        messages=messages
-    )
+        # Define a system message and user message
+        messages = [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": user_question}
+        ]
 
-    # Send the AI response to the user
-    assistant_response = response['choices'][0]['message']['content']
-    message.reply(assistant_response)
+        # Call the OpenAI API to get a response
+        response = openai.ChatCompletion.create(
+            model="gpt-4",  # Use "gpt-4" if you have early access, otherwise "gpt-3.5-turbo"
+            messages=messages
+        )
+
+        # Send the AI response to the user
+        assistant_response = response['choices'][0]['message']['content']
+        message.reply(assistant_response)
+
+        # Increment the request count
+        request_count += 1
+
+    except Exception as e:
+        # Handle any errors that occur during processing
+        error_message = f"An error occurred: {str(e)}"
+        message.reply(error_message)
+
+# Define a command to check the request count
+@Client.on_message(filters.command("request_count", prefixes="/"))
+def check_request_count(client: Client, message: Message):
+    global request_count  # Access the request_count variable
+    message.reply(f"Number of requests: {request_count}")
