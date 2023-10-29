bba53e455508a015ed0548e64eef39ff25c31412
SakuraScripter <148360334+Sakurarendbot@users.noreply.github.com>
2023-10-29 20:49:11 +0530

    Update ai.py

--- plugins/ai.py
+++ plugins/ai.py
@@ -2,13 +2,15 @@ import os
 import openai
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from info import API_ID, API_HASH, BOT_TOKEN, OPENAI_API_KEY
+from info import API_ID, API_HASH, BOT_TOKEN, OPENAI_API_KEY, OPENAI_ORGANIZATION
 
 # Initialize a variable to keep track of the number of requests
 request_count = 0
 
 # Set your OpenAI API key
+
 openai.api_key = OPENAI_API_KEY
+OpenAI-Organization = OPENAI_ORGANIZATION
 
 # Define a command for users to ask questions
 @Client.on_message(filters.command("ask", prefixes="/"))
