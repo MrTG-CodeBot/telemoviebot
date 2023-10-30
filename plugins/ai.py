17e448b446ebc0d2d8d89f1d1ea0867c4aa55698
SakuraScripter <148360334+Sakurarendbot@users.noreply.github.com>
2023-10-30 11:47:34 +0530

    Update ai.py

--- plugins/ai.py
+++ plugins/ai.py
@@ -8,23 +8,28 @@ GOOGLE_BARD_API_KEY = os.environ.get('AIzaSyBHHKi9eI9NSoPb_MMjsYSTGqE-bFPU2DE')
 
 @Client.on_message(filters.command("ai"))
 async def ai(client, message):
-    # Get the user's message text.
-    message_text = message.text
+  # Get the user's message text.
+  message_text = message.text
 
-    # Send the request to the Bard API
-    response = await client.send_request(
-        "https://language.googleapis.com/v1beta1/generation/",
-        method="POST",
-        data={"input_text": request},
-    )
+  # Send the request to the Bard API
+  response = await client.send_message(
+    message.chat.id,
+    text="**Wait for the response from Sakura AI...**",
+  )
 
+  # Generate a response using Google Bard.
+  bard_response = await bard_client.generate(
+    prompt=message_text,
+    max_tokens=1024,
+    temperature=0.7
+  )
 
-    # Generate a response using Google Bard.
-    response = await bard_client.generate(
-        prompt=message_text,
-        max_tokens=1024,
-        temperature=0.7
-    )
+  # Edit the original message with the response from Google Bard.
+  await client.edit_message_text(
+    message.chat.id,
+    response.message_id,
+    text=bard_response,
+ )
 
-    # Send the response to the user.
-    await message.reply_text(response)
+ # Send the response to the user.
+  await message.reply_text(response)
