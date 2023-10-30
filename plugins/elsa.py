972436d387ed3a5e82e4e3ba5b49fe8443b73311
SakuraScripter <148360334+Sakurarendbot@users.noreply.github.com>
2023-10-30 12:45:07 +0530

    Create elsa.py

---
+++ plugins/elsa.py
@@ -0,0 +1,23 @@
+import os
+import pyrogram
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from info import API_ID, API_HASH, BOT_TOKEN, GOOGLE_BARD_API_KEY
+
+GOOGLE_BARD_API_KEY = os.environ.get('AIzaSyBHHKi9eI9NSoPb_MMjsYSTGqE-bFPU2DE')
+
+@Client.on_message(filters.command("ai"))
+async def ai(client, message):
+
+    # Get the user's message text.
+    message_text = message.text
+
+    # Generate a response using Google Bard.
+    bard_response = await bard_client.generate(
+        prompt=message_text,
+        max_tokens=1024,
+        temperature=0.7
+    )
+
+    # Send the response to the user.
+    await message.reply_text(bard_response)
