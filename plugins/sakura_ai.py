f9d67a3a0462b15c560e4b4b9127b8716604a361
SakuraScripter <148360334+Sakurarendbot@users.noreply.github.com>
2023-10-31 10:34:01 +0530

    Update sakura_ai.py

--- plugins/sakura_ai.py
+++ plugins/sakura_ai.py
@@ -6,9 +6,6 @@ from info import API_ID, API_HASH, BOT_TOKEN, BARD_AI_API_KEY
 # Create a new Bard AI instance.
 bard = Bard(api_key="YOUR_BARD_AI_API_KEY")
 
-# Create a new Telegram bot client.
-Client("sakura_ai")
-
 # Define a function that will be called when the bot receives the `/sakura_ai` command.
 async def sakura_ai_command(client: Client, message: pyrogram.types.Message):
     """Command handler for the `/sakura_ai` command."""
@@ -23,5 +20,5 @@ async def sakura_ai_command(client: Client, message: pyrogram.types.Message):
     await message.reply_text(response)
 
 # Register the command handler with the Telegram bot client.
-Client.add_handler(sakura_ai_command)
+@Client.add_handler(sakura_ai_command)
 
