46c32069e57fa8d69abea3cb007c66927aee88fe
SakuraScripter <148360334+Sakurarendbot@users.noreply.github.com>
2023-11-01 01:24:05 +0530

    Update bard.py

--- plugins/bard.py
+++ plugins/bard.py
@@ -8,37 +8,36 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from info import API_ID, API_HASH, BOT_TOKEN, BARD_API_TOKEN
 
-bard_api_token = 'BARD_API_TOKEN'
+bard_api_token = BARD_API_TOKEN
 
-Client('bot', api_id, api_hash, bot_token)
-bard = bardapi.core.Bard(bard_api_token)
+Client('bot', API_ID, API_HASH, BOT_TOKEN)
+bard = Bard(bard_api_token)
 
 # Add a command handler for the `/bard` command.
 @Client.on_message(filters.command(["bard"]) & filters.text)
 async def bard_ai(client: Client, message: Message):
- bard_status = bard.get_service_status()
-if bard_response['status'] != 'success':
-  await message.reply_text('The Bard AI service is currently unavailable.')
-  return
+    bard_response = bard.get_service_status()
+    if bard_response['status'] != 'success':
+        await message.reply_text('The Bard AI service is currently unavailable.')
+        return
 
- prompt = message.text[len('/bard'):].strip()
+    prompt = message.text[len('/bard'):].strip()
 
- response = bard.get_answer(prompt)
+    response = bard.get_answer(prompt)
 
- if response['status'] == 'success':
-  # Success!
-  generated_text = response['answer']
+    if response['status'] == 'success':
+        # Success!
+        generated_text = response['answer']
 
-  await message.edit_text(generated_text)
+        await message.edit_text(generated_text)
 
- else:
-  # Error!
-  await message.reply_text('An error occurred while processing your request.')
+    else:
+        # Error!
+        await message.reply_text('An error occurred while processing your request.')
 
-
+# Handle all messages, regardless of whether they start with `/bard`.
 @Client.on_message()
 async def handle_message(client: Client, message: Message):
-  if not message.text.startswith('/bard'):
-  return
+    if message.text.startswith('/bard'):
+        await bard_ai(client, message)
 
- await bard_ai(client, message)
