501f67e0451ab87613d67977c425785c3b70e6ea
SakuraScripter <148360334+Sakurarendbot@users.noreply.github.com>
2023-10-31 10:20:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -46,7 +46,7 @@ API_HASH = os.environ.get('API_HASH', '652bae601b07c928b811bdb310fdb4b0')
 BOT_TOKEN = os.environ.get('BOT_TOKEN', '6629383271:AAE1ZdxlW0ZMwbhGNXMdZpCvQZaW4LPDgX8')
 TMDB_API_KEY = os.environ.get('b0d58dcd0ccbe19340aa143daf4c6ad0')
 OMDB_API_KEY = os.environ.get('42d8ac83')
-GOOGLE_BARD_API_KEY = os.environ.get('AIzaSyBHHKi9eI9NSoPb_MMjsYSTGqE-bFPU2DE')
+GENERATIVE_AI_API_KEY = os.environ.get('AIzaSyBHHKi9eI9NSoPb_MMjsYSTGqE-bFPU2DE')
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
