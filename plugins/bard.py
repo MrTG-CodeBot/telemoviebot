f3fd9e1ba3176082b4e152c2fab4d243a7e50b55
SakuraScripter <148360334+Sakurarendbot@users.noreply.github.com>
2023-11-01 15:46:57 +0530

    Update bard.py

--- plugins/bard.py
+++ plugins/bard.py
@@ -6,23 +6,25 @@ from info import API_ID, API_HASH, BOT_TOKEN, OPENAI_API_KEY
 OPENAI_API_KEY = openai.api_key
 openai.api_key = "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"
 
+import curl
+
 async def send_request(payload):
-    async with asyncio.to_thread(curl.post, "https://api.openai.com/v1/chat/completions",
-                                 json=payload, headers={"Authorization": f"Bearer {openai.api_key}"},                         content_type="application/json"):
-        response = await resp.json()
-        return response
+  async with asyncio.to_thread(curl.post, "https://api.openai.com/v1/chat/completions",
+                 json=payload, headers={"Authorization": f"Bearer {openai.api_key}"},            content_type="application/json"):
+    response = await resp.json()
+    return response
 
 async def ask(client, message):
-    text = message.text
+  text = message.text
 
-    payload = {
-        "model": "gpt-3.5-turbo",
-        "messages": [
-            {"role": "user", "content": text},
-        ],
-        "temperature": 0.7,
-    }
+  payload = {
+    "model": "gpt-3.5-turbo",
+    "messages": [
+      {"role": "user", "content": text},
+    ],
+    "temperature": 0.7,
+  }
 
-    response = await send_request(payload)
+  response = await send_request(payload)
 
-    await client.send_message(message.chat.id, response["choices"][0]["text"])
+  await client.send_message(message.chat.id, response["choices"][0]["text"])
