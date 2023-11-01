7d3ce6d4eea2492e43f423055d2ecdede48deae4
SakuraScripter <148360334+Sakurarendbot@users.noreply.github.com>
2023-11-01 19:12:49 +0530

    Update bard.py

--- plugins/bard.py
+++ plugins/bard.py
@@ -1,36 +1,24 @@
 import pyrogram
-import openai
-import asyncio
-from info import API_ID, API_HASH, BOT_TOKEN, OPENAI_API_KEY
+from pyrogram import Client, filters
+from transformers import AutoModelForQuestionAnswering
+from transformers import AutoTokenizer
+from info import API_ID,API_HASH,BOT_TOKEN
 
-OPENAI_API_KEY = openai.api_key
-openai.api_key = "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"
+# Initialize the Pyrogram client
+async with pyrogram.Client("bard-ai-bot") as app:
 
-import curl
+    model = AutoModelForQuestionAnswering.from_pretrained("google/bard")
+    tokenizer = AutoTokenizer.from_pretrained("google/bard")
 
-# Define a command handler for /ask
-@Client.on_message(filters.command("ask", prefixes="/"))
-async def ask_command(client, message):
-    # Call your ask function with the message
-    response = await ask(client, message)
+@Client.on_message(filters.command("ask"))
+ async def ask_command(client, message):
+        question = message.text.split(" ")[1:]
 
-async def send_request(payload):
-  async with asyncio.to_thread(curl.post, "https://api.openai.com/v1/chat/completions",
-                 json=payload, headers={"Authorization": f"Bearer {openai.api_key}"},            content_type="application/json"):
-    response = await resp.json()
-    return response
+        answer = await model.generate(
+            input_ids=tokenizer(question, return_tensors="pt").input_ids,
+            max_length=1024,
+        )[0][0]
 
-async def ask(client, message):
-  text = message.text
+        await message.reply_text(answer)
 
-  payload = {
-    "model": "gpt-3.5-turbo",
-    "messages": [
-      {"role": "user", "content": text},
-    ],
-    "temperature": 0.7,
-  }
-
-  response = await send_request(payload)
-
-  await client.send_message(message.chat.id, response["choices"][0]["text"])
+    
