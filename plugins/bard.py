a2e8175599bea498ad88e71115f3e4295292b79b
SakuraScripter <148360334+Sakurarendbot@users.noreply.github.com>
2023-11-01 19:19:30 +0530

    Update bard.py

--- plugins/bard.py
+++ plugins/bard.py
@@ -8,9 +8,6 @@ API_ID = 8914119
 API_HASH = "652bae601b07c928b811bdb310fdb4b0"
 BOT_TOKEN = "6629383271:AAE1ZdxlW0ZMwbhGNXMdZpCvQZaW4LPDgX8"
 
-# Initialize the Pyrogram client
-async with pyrogram.Client("bard-ai-bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN) as app:
-
     model = AutoModelForQuestionAnswering.from_pretrained("google/bard")
     tokenizer = AutoTokenizer.from_pretrained("google/bard")
 
