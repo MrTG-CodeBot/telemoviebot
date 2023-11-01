91db81731cfa934cc6df244516007b4c3bcbbc4f
SakuraScripter <148360334+Sakurarendbot@users.noreply.github.com>
2023-11-01 09:05:42 +0530

    Update bard.py

--- plugins/bard.py
+++ plugins/bard.py
@@ -1,30 +1,34 @@
 import pyrogram
 from pyrogram import Client
 import openai
+import requests
+from requests import Session
 from info import API_ID, API_HASH, BOT_TOKEN, OPENAAI_API_KEY
 
-openai.api_key = OPENAI_API_KEY
-
-# Your OpenAI API key
-openai.api_key = 'YOUR_OPENAI_API_KEY'
+# Authorize the OpenAI API key
+def authorize_openai(api_key):
+    session = Session()
+    session.headers["Authorization"] = f"Bearer {api_key}"
+    return session
 
 # Define a function to handle incoming messages
 @Client.on_message(pyrogram.filters.command('ask', prefixes='/'))
 async def ask_command(client, message):
-    # Extract the user's question from the command
-    question = ' '.join(message.command[1:])
+  # Extract the user's question from the command
+  question = ' '.join(message.command[1:])
 
-    if not question:
-        await message.reply("Please ask a question using the /ask command.")
-    else:
-        # Generate a response using ChatGPT
-        response = openai.Completion.create(
-            engine='text-davinci-002',
-            prompt=f"I have a question: {question}",
-            max_tokens=150,
-            temperature=0.7
-        )
+  if not question:
+    await message.reply("Please ask a question using the /ask command.")
+  else:
+    # Authorize the OpenAI API key
+    session = authorize_openai(OPENAAI_API_KEY)
 
-        # Send the response to the user
-        await message.reply(response.choices[0].text)
+    # Generate a response using ChatGPT
+    response = session.post(
+      "https://api.openai.com/v1/engines/davinci/completions",
+      headers={"Authorization": f"Bearer {OPENAAI_API_KEY}"},
+      json={"prompt": f"I have a question: {question}", "max_tokens": 150, "temperature": 0.7}
+    )
 
+    # Send the response to the user
+    await message.reply(response.json()["choices"][0]["text"])
