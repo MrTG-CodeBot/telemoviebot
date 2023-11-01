7f95ba63c4bdbcf286ec1feb01dd49899653b194
SakuraScripter <148360334+Sakurarendbot@users.noreply.github.com>
2023-11-01 19:43:42 +0530

    Update bard.py

--- plugins/bard.py
+++ plugins/bard.py
@@ -6,21 +6,23 @@ from transformers import AutoModelForQuestionAnswering
 from transformers import AutoTokenizer
 
 # Define the API credentials
-API_ID = 8914119
+API_ID = "8914119"
 API_HASH = "652bae601b07c928b811bdb310fdb4b0"
 BOT_TOKEN = "6629383271:AAE1ZdxlW0ZMwbhGNXMdZpCvQZaW4LPDgX8"
 
+async def main():
     model = AutoModelForQuestionAnswering.from_pretrained("google/bard")
     tokenizer = AutoTokenizer.from_pretrained("google/bard")
 
-@Client.on_message(filters.command("ask"))
-async def ask_command(client, message):
-    question = message.text.split(" ")[1:]
+    client = Client(BOT_TOKEN)
 
-    answer = await model.generate(
-        input_ids=tokenizer(question, return_tensors="pt").input_ids,
-        max_length=1024,
-    )[0][0]
+    @client.on_message(filters.command("ask"))
+    async def ask_command(client, message):
+        question = message.text.split(" ")[1:]
 
-    await message.reply_text(answer)
+        answer = await model.generate(
+            input_ids=tokenizer(question, return_tensors="pt").input_ids,
+            max_length=1024,
+        )[0][0]
 
+        await message.reply_text(answer)
