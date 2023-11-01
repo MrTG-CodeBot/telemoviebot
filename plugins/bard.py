b36164d1c3eb9be8400caa189374d206d13cc796
SakuraScripter <148360334+Sakurarendbot@users.noreply.github.com>
2023-11-01 09:31:44 +0530

    Update bard.py

--- plugins/bard.py
+++ plugins/bard.py
@@ -20,22 +20,22 @@ def authorize_openai(api_key):
 @Client.on_message(pyrogram.filters.command('ask', prefixes='/'))
 async def ask_command(client, message):
     
-  # Extract the user's question from the command
+# Extract the user's question from the command
   question = ' '.join(message.command[1:])
 
   if not question:
     await message.reply("Please ask a question using the /ask command.")
   else:
       
-    # Authorize the OpenAI API key
+# Authorize the OpenAI API key
     session = authorize_openai(OPENAAI_API_KEY)
 
-    # Generate a response using ChatGPT
+# Generate a response using ChatGPT
     response = session.post(
       "https://api.openai.com/v1/engines/davinci/completions",
       headers={"Authorization": f"Bearer {OPENAAI_API_KEY}"},
       json={"prompt": f"I have a question: {question}", "max_tokens": 150, "temperature": 0.7}
     )
 
-    # Send the response to the user
+# Send the response to the user
     await message.reply(response.json()["choices"][0]["text"])
