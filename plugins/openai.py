0e871c7ee4983db62ae50388f396982bb8e32178
SakuraScripter <148360334+Sakurarendbot@users.noreply.github.com>
2023-11-02 07:52:51 +0530

    Update openai.py

--- plugins/openai.py
+++ plugins/openai.py
@@ -4,59 +4,41 @@ from pyrogram import Client, filters
 import openai
 import re
 
-OPENAI_API_KEY = os.environ.get('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')
+# Set your OpenAI API key here
+OPENAI_API_KEY = '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'
 openai.api_key = OPENAI_API_KEY
 
 # Authentication code filter
-def is_authenticated(message):
-  return message.from_user.is_authenticated
+def is_authenticated(client, message):
+    return client.get_chat_member(message.chat.id, message.from_user.id).status in ("member", "admin", "creator")
 
 # OpenAI command handler
-@Client.on_message(filters.command("openai") and is_authenticated)
+@Client.on_message(filters.command("openai") & is_authenticated)
 async def openai_command(client, message):
-  text = message.text.split(" ")[1:]
+    text = " ".join(message.text.split(" ")[1:])  # Join the text parts after the command
 
-  # Try to generate a completion from OpenAI
-  try:
-    response = openai.Completion.create(
-      engine="davinci",
-      prompt=text,
-      max_tokens=1024,
-      temperature=0.7,
-      top_p=1,
-      frequency_penalty=0,
-      presence_penalty=0,
-    )
-  except Exception as e:
-    # If an error occurs, send a message to the user
-    await Client.send_message(message.chat.id, f"Error: {e}")
-  else:
-    # If the completion was successful, send the completion to the user
-    if response:
-      await Client.send_message(message.chat.id, response.choices[0].text)
+    # Try to generate a completion from OpenAI
+    try:
+        response = openai.Completion.create(
+            engine="davinci",
+            prompt=text,
+            max_tokens=1024,
+            temperature=0.7,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+        )
+    except Exception as e:
+        # If an error occurs, send a message to the user
+        await message.reply(f"Error: {e}")
     else:
-      await Client.send_message(message.chat.id, "OpenAI failed to generate a completion.")
+        # If the completion was successful, send the completion to the user
+        if response and response.choices:
+            await message.reply(response.choices[0].text)
+        else:
+            await message.reply("OpenAI failed to generate a completion.")
 
-  # Send the response back to the user
-  return {
-    "id": "chatcmpl-abc123",
-    "object": "chat.completion",
-    "created": 1677858242,
-    "model": "gpt-3.5-turbo-0613",
-    "usage": {
-      "prompt_tokens": 13,
-      "completion_tokens": 7,
-      "total_tokens": 20
-    },
-    "choices": [
-      {
-        "message": {
-          "role": "assistant",
-          "content": "\n\nThis is a test!"
-        },
-        "finish_reason": "stop",
-        "index": 0
-      }
-    ]
-  }
+app = Client("my_bot")
 
+if __name__ == "__main__":
+    app.run()
