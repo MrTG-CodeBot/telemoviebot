519ab181d9e98e39d9d490d27c459ec3bbcbe00e
MrTG-CodeBot <148360334+MrTG-CodeBot@users.noreply.github.com>
2023-11-08 21:03:48 +0530

    Update bard.py

--- plugins/bard.py
+++ plugins/bard.py
@@ -1,5 +1,10 @@
 import asyncio
+import os
+from os import environ
 from pyrogram import Client, filters
+
+os.environ["SECURE_1PSID"] = "cwhU4YcCopoMy_3kxUtz-RW9qhycZAPMGN6lOic3SNCmbA-DwVQ8HRfq307q7bxrhTGeVw."
+os.environ["SECURE_1PSIDTS"] = "sidts-CjEBNiGH7iFBn8ExXuBa7bjk6LTA7R1P1DzgWh7Cto_BmkM7MyOLBdtWqm9PWMUGyG2uEAA"
 
 @Client.on_message(filters.command("bard", prefixes="!"))
 async def command(client, message):
@@ -19,5 +24,3 @@ async def command(client, message):
 
             response = app.send_message("Bard", prompt)
             print(f"Bard: {response.text}")
-
-app.run()
