6c7f592950da60aa10510bf85ea42d1ed74461c8
MrTG-CodeBot <148360334+MrTG-CodeBot@users.noreply.github.com>
2023-11-09 19:08:28 +0530

    Update sub.py

--- plugins/sub.py
+++ plugins/sub.py
@@ -4,35 +4,33 @@ from pyrogram import Client, filters
 from pysrt import SubRipFile
 from info import API_ID, API_HASH, BOT_TOKEN
 
-@client.on_message(filters.command("subtitle"))
+@Client.on_message(filters.command("subtitle"))
 async def subtitle(client, message):
-  if message.text.startswith('/subtitle'):
-    pass
+    movie_name, year = message.text.split()[1:]
+    subtitles_url = await get_subtitle_url(movie_name, year)
+    subtitle_file = await download_subtitle(subtitles_url)
+    await client.send_document(chat_id=message.chat.id, document=open(subtitle_file, 'rb'))
 
-  async def get_subtitle_url(self, movie_name, year):
+async def get_subtitle_url(movie_name, year):
+    response = requests.get('https://api.opensubtitles.org/search/subtitles', params={
+        'q': movie_name,
+        'y': year
+    })
 
-    response = requests.get('https://api.opensubtitles.org/search/subtitles', params={
-      'q': movie_name,
-      'y': year
-    })
+    if response.status_code != 200:
+        raise Exception('Failed to get subtitles URL')
 
-    if response.status_code != 200:
-      raise Exception('Failed to get subtitles URL')
+    subtitles_list = response.json()['data']
+    return subtitles_list[0]['zip_download_link']
 
-    subtitles_list = response.json()['data']
+async def download_subtitle(subtitles_url):
+    try:
+        response = requests.get(subtitles_url)
 
-    return subtitles_list[0]['zip_download_link']
+        with open('subtitles.srt', 'wb') as f:
+            f.write(response.content)
+    finally:
+        if f:
+            f.close()
 
-  async def download_subtitle(self, subtitles_url):
-  import requests
-
-  try:
-    response = requests.get(subtitles_url)
-
-    with open('subtitles.srt', 'wb') as f:
-      f.write(response.content)
-  finally:
-    if f:
-      f.close()
-
-  return 'subtitles.srt'
+    return 'subtitles.srt'
