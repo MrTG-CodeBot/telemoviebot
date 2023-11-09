b1803c243dae41133f520fc336b91614c03ddaf0
MrTG-CodeBot <148360334+MrTG-CodeBot@users.noreply.github.com>
2023-11-09 20:23:58 +0530

    Update anime.py

--- plugins/anime.py
+++ plugins/anime.py
@@ -4,16 +4,19 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.command("generate"))
 async def generate(client, message):
-    response = requests.get('https://thisanimedoesnotexist.ai/results?seed=0.1234567890123456')
-    if response.status_code != 200:
-        await message.reply_text("Failed to generate an image. Please try again later.")
+    try:
+        response = requests.get('https://thisanimedoesnotexist.ai/results?seed=0.1234567890123456')
+        response.raise_for_status()
+    except requests.exceptions.RequestException as err:
+        await message.reply_text(f"Failed to generate an image. Error: {err}")
         return
 
     file_name = f"anime_character_{message.message_id}.jpg"
     try:
         with open(file_name, 'wb') as file:
             file.write(response.content)
-        await client.send_photo(chat_id=message.chat.id, photo=open(file_name, 'rb'))
+        with open(file_name, 'rb') as img:
+            await client.send_photo(chat_id=message.chat.id, photo=img)
     except Exception as e:
         await message.reply_text(f"An error occurred: {str(e)}")
     finally:
