da18db8b746aa036a0eb2fa38bd61941499bd6bf
MrTG-CodeBot <148360334+MrTG-CodeBot@users.noreply.github.com>
2023-11-09 18:48:34 +0530

    Update sub.py

--- plugins/sub.py
+++ plugins/sub.py
@@ -6,44 +6,44 @@ from info import API_ID, API_HASH, BOT_TOKEN
 
 @client.on_message(filters.command("subtitle"))
 async def subtitle(client, message):
-  if message.text.startswith('/subtitle'):
-    movie_name = message.text.split()[1]
-       year = message.text.split()[2]
+  if message.text.startswith('/subtitle'):
+    movie_name = message.text.split()[1]
+    year = message.text.split()[2]
 
-            try:
+    try:
+      subtitles_url = await self.get_subtitle_url(movie_name, year)
 
-                subtitles_url = await self.get_subtitle_url(movie_name, year)
+      subtitle_file = await self.download_subtitle(subtitles_url)
 
-                subtitle_file = await self.download_subtitle(subtitles_url)
+      await self.send_document(message.chat.id, subtitle_file)
+    except Exception as e:
+      print(e)
+      await self.send_message(message.chat.id, f"Failed to download subtitles for {movie_name}: {e}")
 
-                await self.send_document(message.chat.id, subtitle_file)
-            except Exception as e:
+  async def get_subtitle_url(self, movie_name, year):
 
-                print(e)
-                await self.send_message(message.chat.id, f"Failed to download subtitles for {movie_name}: {e}")
+    response = requests.get('https://api.opensubtitles.org/search/subtitles', params={
+      'q': movie_name,
+      'y': year
+    })
 
-    async def get_subtitle_url(self, movie_name, year):
-       
+    if response.status_code != 200:
+      raise Exception('Failed to get subtitles URL')
 
-        response = requests.get('https://api.opensubtitles.org/search/subtitles', params={
-            'q': movie_name,
-            'y': year
-        })
+    subtitles_list = response.json()['data']
 
-        if response.status_code != 200:
-            raise Exception('Failed to get subtitles URL')
+    return subtitles_list[0]['zip_download_link']
 
-        subtitles_list = response.json()['data']
+  async def download_subtitle(self, subtitles_url):
+  import requests
 
-        return subtitles_list[0]['zip_download_link']
+  try:
+    response = requests.get(subtitles_url)
 
-    async def download_subtitle(self, subtitles_url):
-        import requests
+    with open('subtitles.srt', 'wb') as f:
+      f.write(response.content)
+  finally:
+    if f:
+      f.close()
 
-        response = requests.get(subtitles_url)
-
-        with open('subtitles.srt', 'wb') as f:
-            f.write(response.content)
-
-        return 'subtitles.srt'
-
+  return 'subtitles.srt'
