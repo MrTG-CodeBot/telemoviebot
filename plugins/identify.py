433b1d8ac4aa0bbac16678bcc52115051939ea24
MrTG-CodeBot <148360334+MrTG-CodeBot@users.noreply.github.com>
2023-11-12 12:36:54 +0530

    Update identify.py

--- plugins/identify.py
+++ plugins/identify.py
@@ -1,12 +1,7 @@
 import requests
 from pyrogram import Client, filters
-import acrcloud
-import logging
+from acrcloud_sdk_python.acrcloud_recognizer import ACRCloudRecognizer
 from info import API_ID, API_HASH, BOT_TOKEN
-
-# Set up logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 config = {
     'host':'identify-ap-southeast-1.acrcloud.com',
@@ -14,18 +9,16 @@ config = {
     'secret':'XgSS5vWJ172QYhulX9WABchXNekBflz6mei5bJCy',
 }
 
-acrcloud_client = acrcloud.ACRCloudRecognizer(config)
+acrcloud_client = ACRCloudRecognizer(config)
 
 @Client.on_message(filters.voice | filters.audio)
 async def song_recognizer(client, message):
-    try:
-        song = await message.download()
-        result = acrcloud_client.recognize_by_file(song, 0)
-        if 'metadata' in result and 'music' in result['metadata']:
-            song_info = result['metadata']['music'][0]
-            song_name = song_info['title']
-            artist_name = song_info['artists'][0]['name']
-            await message.reply_text(f"Song: {song_name}\nArtist: {artist_name}")
+    song = await message.download()
+    result = acrcloud_client.recognize_by_file(song, 0)
+    song_info = result['metadata']['music'][0]
+    song_name = song_info['title']
+    artist_name = song_info['artists'][0]['name']
+    await message.reply_text(f"Song: {song_name}\nArtist: {artist_name}")
         else:
             logger.error(f"No music data in the recognition result: {result}")
             await message.reply_text("Sorry, I couldn't recognize the song.")
