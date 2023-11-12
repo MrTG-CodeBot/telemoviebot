a76f974df2a97e47a9cbdb3659f6231039e721df
MrTG-CodeBot <148360334+MrTG-CodeBot@users.noreply.github.com>
2023-11-12 13:25:43 +0530

    Update identify.py

--- plugins/identify.py
+++ plugins/identify.py
@@ -1,42 +1,35 @@
-import logging
+import requests
 from pyrogram import Client, filters
-from acrcloud.recognizer import ACRCloudRecognizer
+import acrcloud
 from info import API_ID, API_HASH, BOT_TOKEN
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
+# ACRCloud configuration for song recognition
 config = {
     'host': 'identify-ap-southeast-1.acrcloud.com',
     'key': '0db9a34202c7797b535cba436dc24d07',
     'secret': 'XgSS5vWJ172QYhulX9WABchXNekBflz6mei5bJCy',
 }
 
-acrcloud_client = ACRCloudRecognizer(config)
+# Establishing a connection with ACRCloud service
+acrcloud_client = acrcloud.ACRCloudRecognizer(config)
 
-
+# Function to recognize songs from voice/audio messages
 @Client.on_message(filters.voice | filters.audio)
-async def song_recognizer(client, message):
-    try:
-        song = await message.download()
-        result = acrcloud_client.recognize_by_file(song, 0)
+async def recognize_song(client, message):
+    # Download the received audio file
+    audio_file = await message.download()
+    
+    # Use ACRCloud to recognize the song from the audio file
+    result = acrcloud_client.recognize_by_file(audio_file, 0)
+    
+    # Extract song information if recognition is successful
+    if 'metadata' in result and 'music' in result['metadata']:
+        song_info = result['metadata']['music'][0]
+        song_name = song_info['title']
+        artist_name = song_info['artists'][0]['name']
         
-        if 'metadata' in result and 'music' in result['metadata']:
-            song_info = result['metadata']['music'][0]
-            song_name = song_info.get('title')
-            artist_name = song_info['artists'][0]['name']
-            if song_name and artist_name:
-                await message.reply_text(f"Song: {song_name}\nArtist: {artist_name}")
-            else:
-                logger.error(f"Incomplete music data in the recognition result: {result}")
-                await message.reply_text("Sorry, I couldn't recognize the song.")
-        else:
-            logger.error(f"No music data in the recognition result: {result}")
-            await message.reply_text("Sorry, I couldn't recognize the song.")
-    except Exception as e:
-        logger.error(f"An error occurred while recognizing the song: {e}")
-        await message.reply_text("Sorry, an error occurred while recognizing the song.")
-    finally:
-        if song:
-            os.remove(song)
-
+        # Respond with recognized song and artist information
+        await message.reply_text(f"Song: {song_name}\nArtist: {artist_name}")
+    else:
+        # Handle cases where no song is recognized
+        await message.reply_text("Sorry, I couldn't recognize the song.")
