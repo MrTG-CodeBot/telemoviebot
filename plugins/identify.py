595965e12505d47976abf141b28b143e44412bfb
MrTG-CodeBot <148360334+MrTG-CodeBot@users.noreply.github.com>
2023-11-12 11:35:38 +0530

    Update identify.py

--- plugins/identify.py
+++ plugins/identify.py
@@ -1,40 +1,85 @@
-import os
-import uuid
+import asyncio
+import logging
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from pydub import AudioSegment
-import speech_recognition as sr
 from info import API_ID, API_HASH, BOT_TOKEN
+import acrcloud
+import ffmpeg
 
+config = {
+    'host': 'host',
+    'key': 'api_key',
+    'secret': 'api_secret',
+}
 
-@Client.on_message(filters.voice)
-async def handle_voice_message(client, message):
-    voice_message = await message.download()
-    voice_segment = AudioSegment.from_file(voice_message)
-    voice_file_path = f"{uuid.uuid4()}.mp3"
-    voice_segment.export(voice_file_path, format="mp3")
-    song_text = transcribe_audio(voice_file_path)
-    if song_text:
-        keyboard = InlineKeyboardMarkup([[InlineKeyboardButton("Listen on Spotify", url=f"https://open.spotify.com/search?q={song_text}")]])
-        await message.reply_text(f"Song: {song_text}", reply_markup=keyboard)
-    else:
-        await message.reply_text("Sorry, I couldn't identify the song.")
-    os.remove(voice_file_path)
+acrcloud_client = acrcloud.ACRCloudRecognizer(config)
 
-@Client.on_message(filters.video)
-async def handle_video_message(client, message):
-    if message.video.file_size > 5 * 1024 * 1024:
-        await message.reply_text("Sorry, the video file is too large to process.")
-        return
-    video_file = await message.download()
-    audio_segment = AudioSegment.from_file(video_file)
-    voice_file_path = f"{uuid.uuid4()}.mp3"
-    audio_segment.export(voice_file_path, format="mp3")
-    song_text = transcribe_audio(voice_file_path)
-    if song_text:
-        keyboard = InlineKeyboardMarkup([[InlineKeyboardButton("Listen on Spotify", url=f"https://open.spotify.com/search?q={song_text}")]])
-        await message.reply_text(f"Song: {song_text}", reply_markup=keyboard)
-    else:
-        await message.reply_text("Sorry, I couldn't identify the song.")
-    os.remove(voice_file_path)
+def convert_to_mp3(audio_file):
+    logging.info(f"Converting audio file to MP3: {audio_file}")
+    try:
+        ffmpeg.input(audio_file).output(audio_file.replace('.wav', '.mp3')).run()
+    except Exception as e:
+        logging.error(f"Failed to convert audio file: {e}")
+        return None
 
+def recognize_song(audio_file):
+    logging.info(f"Recognizing song from WAV file: audio.wav")
+    try:
+        result = acrcloud_client.recognize_by_file('audio.wav', 0)
+        return result
+    except Exception as e:
+        logging.error(f"Failed to recognize song: {e}")
+        return None
+
+def estimate_song_duration(audio_file):
+    logging.info(f"Estimating song duration: {audio_file}")
+    try:
+        probe = ffmpeg.probe(audio_file)
+        duration = probe['format']['duration']
+        return duration
+    except Exception as e:
+        logging.error(f"Failed to estimate song duration: {e}")
+        return None
+
+async def song_recognizer(client, message):
+    # Download the audio file
+    logging.info(f"Downloading audio file: {message.id}")
+    audio_file = await message.download()
+
+    # Convert the audio file to WAV format if necessary
+    if audio_file.endswith('.mp3') or audio_file.endswith('.ogg'):
+        logging.info(f"Converting audio file to WAV: {audio_file}")
+        converted_audio_file = await asyncio.to_thread(convert_to_wav, audio_file)
+        if converted_audio_file is None:
+            return
+
+        audio_file = converted_audio_file
+
+    # Estimate the song duration
+    duration = await asyncio.to_thread(estimate_song_duration, audio_file)
+
+    # Recognize the song using ACRCloud
+    result = await asyncio.to_thread(recognize_song, audio_file)
+
+    # Reply with the song recognition result
+    if result:
+        await message.reply_text(result)
+
+    # Convert the recognized song to MP3
+    if result:
+        logging.info(f"Converting recognized song to MP3: audio.wav")
+        try:
+            ffmpeg.input(audio_file).output(f"{result['id']}.mp3").run()
+        except Exception as e:
+            logging.error(f"Failed to convert recognized song: {e}")
+
+    # Delete the temporary WAV file
+    if audio_file.endswith('.wav'):
+        logging.info(f"Deleting temporary WAV file: audio.wav")
+        try:
+            os.remove('audio.wav')
+        except Exception:
+            pass
+
+    # Inform user about song duration if available
+    if duration:
+        await message.reply_text(f"Song duration: {duration} seconds")
