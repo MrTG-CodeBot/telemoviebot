09bb32892ca8e110f1ae899e6c064bab88f9af85
MrTG-CodeBot <148360334+MrTG-CodeBot@users.noreply.github.com>
2023-11-15 19:34:27 +0530

    Update rename.py

--- plugins/rename.py
+++ plugins/rename.py
@@ -1,35 +1,47 @@
 import pyrogram
-from pyrogram import Client, filters, types
+from pyrogram import Client, InlineKeyboardMarkup, InlineKeyboardButton
 
-async def rename_document(message: types.Message):
-    # Get the document file from the message
-    document = message.document
+@Client.on_message(filters.command("rename") & filters.document)
+def rename_document(client, message):
+    file_id = message.document.file_id
+    file_name = message.document.file_name
 
-    # Get the current file name
+    # Create an inline keyboard with two buttons: "Rename" and "Cancel"
+    keyboard = InlineKeyboardMarkup(
+        [[
+            InlineKeyboardButton("Rename", callback_data=f"rename_{file_id}"),
+            InlineKeyboardButton("Cancel", callback_data="cancel"),
+        ]]
+    )
+
+    # Send the inline keyboard to the user
+    message.reply_text("Select an option:", reply_markup=keyboard)
+
+@Client.on_callback_query(filters.regex(r"^rename_\d+$"))
+def handle_rename_callback(client, callback_query):
+    file_id = int(callback_query.data.split("_")[1])
+
+# Get the current file name
     current_filename = document.file_name
 
     # Ask the user for the new file name
     await message.reply_text(f"Current file name: {current_filename}")
-    await message.reply_text("Enter the new file name:")
 
-    # Get the new file name from the user
-    new_filename = await app.wait_for_message(filters.text)
+    # Ask the user for the new file name
+    message = callback_query.message.edit_text("Enter the new file name:")
 
-    # Rename the file
-    await app.download_media(document.file_id, file_name=new_filename)
+    # Wait for the user to reply with the new file name
+    @Client.on_message(filters.from_user(callback_query.from_user.id))
+    def handle_new_file_name(client, message):
+        new_file_name = message.text
 
-    # Send a message to the user indicating that the file has been renamed
-    await message.reply_text(f"File renamed to: {new_filename}")
+        # Try to rename the file
+        try:
+            client.download_media(file_id, file_name=new_file_name)
+            message.reply_text(f"File renamed to {new_file_name}")
+        except Exception as e:
+            message.reply_text(f"Failed to rename file: {e}")
 
-# Define a command handler for the `/rename_document` command
-@Client.on_message(filters.command("rename_document"))
-async def handle_rename_document_command(message: types.Message):
-    if not message.reply_to_message:
-        await message.reply_text("Please reply to a document file to rename it.")
-        return
+        # Remove the inline keyboard
+        callback_query.message.edit_text("File renamed")
 
-    # Check if the replied message is a document file
-    if message.reply_to_message.document:
-        await rename_document(message.reply_to_message)
-    else:
-        await message.reply_text("Please reply to a document file to rename it.")
