14f73fe70cfe972a61f569fc870b9071649de707
MrTG-CodeBot <148360334+MrTG-CodeBot@users.noreply.github.com>
2023-11-16 21:44:19 +0530

    Update rename.py

--- plugins/rename.py
+++ plugins/rename.py
@@ -1,75 +1,77 @@
 import pyrogram
 import logging
-from pyrogram import Client, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery 
-from pyrogram.errors import FloodWait
-import asyncio
-from info import API_ID, API_HASH, BOT_TOKEN
+import os
 import tqdm
+from pyrogram import Client, filters
+from pyrogram.errors import FloodWait
+from pyrogram.types import CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
+from info import API_ID, API_HASH, BOT_TOKEN
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-@Client.on_message(pyro.filters.command("rename") & pyro.filters.document)
+
+# Use a list to keep track of ongoing file renames
+ongoing_renames = {}
+
+# Define the command handler
+@Client.on_message(pyrogram.filters.command("rename") & pyrogram.filters.document)
 async def rename_document(client, message):
-    file_id = message.document.file_id
-    file_name = message.document.file_name
+    file_id = message.document.file_id
+    file_name = message.document.file_name
 
-    logging.info(f"Initiating file rename for file_id: {file_id}, file_name: {file_name}")
+    logging.info(f"Initiating file rename for file_id: {file_id}, file_name: {file_name}")
 
-    if message.document.file_size > 2097152000:
-      await message.reply_text("Sorry, files larger than 2 GB cannot be renamed.")
-      return
+    if message.document.file_size > 2097152000:
+        await message.reply_text("Sorry, files larger than 2 GB cannot be renamed.")
+        return
 
-    keyboard = InlineKeyboardMarkup(
-      [[
-        InlineKeyboardButton("Rename", callback_data=f"rename_{file_id}"),
-        InlineKeyboardButton("Cancel", callback_data="cancel"),
-      ]]
-    )
+    keyboard = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton("Rename", callback_data=f"rename_{file_id}"),
+                InlineKeyboardButton("Cancel", callback_data="cancel"),
+            ]
+        ]
+    )
 
-    message = await message.reply_text("Select an option:", reply_markup=keyboard)
-  except Exception as e:
-    logging.error(f"Error during file rename initiation: {e}")
-    await message.reply_text(f"Failed to initiate file rename: {e}")
+    await message.reply_text("Select an option:", reply_markup=keyboard)
 
-@Client.on_callback_query(pyro.filters.regex(r"^rename_\d+$"))
+# Define the callback query handler
+@Client.on_callback_query(pyrogram.filters.regex(r"^rename_\d+$"))
 async def handle_rename_callback(client, callback_query):
-    file_id = int(callback_query.data.split("_")[1])
+    file_id = int(callback_query.data.split("_")[1])
 
-    current_filename = await client.get_file_info(file_id).file_name
+    current_filename = await client.get_file_info(file_id).file_name
 
-    await callback_query.message.edit_text(f"Current file name: {current_filename}")
+    await callback_query.message.edit_text(f"Current file name: {current_filename}")
 
-    message = await callback_query.message.edit_text("Enter the new file name:")
-  except Exception as e:
-    logging.error(f"Error during handling rename callback: {e}")
-    await callback_query.message.edit_text(f"Failed to handle rename callback: {e}")
+    message = await callback_query.message.edit_text("Enter the new file name:")
 
-@Client.on_message(pyro.filters.chat(callback_query.from_user.chat.id) & pyro.filters.from_user(callback_query.from_user.id))
+# Define the message handler for receiving the new file name
+@Client.on_message(pyrogram.filters.text & pyrogram.filters.chat)
 async def handle_new_file_name(client, message):
-    new_file_name = message.text
+    user_id = message.from_user.id
+    chat_id = message.chat.id
 
-    total_size = message.document.file_size
-    with tqdm.tqdm(total=total_size) as pbar:
-        document = await client.download_media(file_id, progress_callback=lambda x: pbar.update(x))
-      except FloodWaitError as e:
-        logging.warning(f"FloodWaitError during file download: {e}")
-        await asyncio.sleep(e.x)
-        document = await client.download_media(file_id, progress_callback=lambda x: pbar.update(x))
+    if user_id in ongoing_renames and chat_id in ongoing_renames[user_id]:
+        file_id, current_filename = ongoing_renames[user_id][chat_id]
+        new_file_name = message.text
 
-  os.rename(document.file_name, new_file_name)
+        try:
+            total_size = message.document.file_size
+            with tqdm.tqdm(total=total_size) as pbar:
+                document = await client.download_media(file_id, progress_callback=lambda x: pbar.update(x))
+        except FloodWait as e:
+            logging.warning(f"FloodWaitError during file download: {e}")
+            await asyncio.sleep(e.x)
+            document = await client.download_media(file_id, progress_callback=lambda x: pbar.update(x))
 
-  total_size = os.path.getsize(new_file_name)
-  with tqdm.tqdm(total=total_size) as pbar:
-      await client.send_document(message.chat.id, new_file_name, caption=f"File renamed to {new_file_name}", progress_callback=lambda x: pbar.update(x))
-    except FloodWaitError as e:
-      logging.warning(f"FloodWaitError during file upload: {e}")
-      await asyncio.sleep(e.x)
-      await client.send_document(message.chat.id, new_file_name, caption=f"File renamed to {new_file_name}", progress_callback=lambda x: p
+        os.rename(document, new_file_name)
 
-    await callback_query.message.edit_text(f"File renamed to {new_file_name}", reply_markup=None)
-except Exception as e:
-    logging.error(f"Failed to rename file: {e}")
+        total_size = os.path.getsize(new_file_name)
+        with tqdm.tqdm(total=total_size) as pbar:
+            await client.send_document(chat_id, new_file_name, caption=f"File renamed to {new_file_name}", progress_callback=lambda x: pbar.update(x))
+    else:
+        logging.error("File rename not initiated or already completed.")
 
-
-  
