163d1435edb709325541ced442f75b4975bb335b
MrTG-CodeBot <148360334+MrTG-CodeBot@users.noreply.github.com>
2023-11-17 23:10:32 +0530

    Update owner.py

--- plugins/owner.py
+++ plugins/owner.py
@@ -9,10 +9,8 @@ from info import API_ID, API_HASH, BOT_TOKEN  # Import the info module
 @Client.on_message(filters.command("about") & filters.incoming & filters.private)
 async def about(client, message):
     await message.reply(ABOUT_TXT)
-    buttons = [
-        [
-            InlineKeyboardButton('<^ ~ ^> ᴍʀ.ʙᴏᴛ ᵀᴳ </>', url="https://t.me/MrTG_Coder"),
-        ]
-    ]
+    buttons = [[
+            InlineKeyboardButton('<^ ~ ^> ᴍʀ.ʙᴏᴛ ᵀᴳ </>', url="https://t.me/MrTG_Coder")
+    ]]
     reply_markup = InlineKeyboardMarkup(buttons)
-    await message.reply_text("More about this bot:", reply_markup=reply_markup)
+    await message.reply_text("<b>Hᴇʟʟᴏ {}.\nMʏ Nᴀᴍᴇ Is <a href=https://t.me/{}>{}</a>.\nI Cᴀɴ Pʀᴏᴠɪᴅᴇ Mᴏᴠɪᴇ Fᴏʀ Yᴏᴜ Jᴜsᴛ Aᴅᴅ Mᴇ Iɴ Yᴏᴜʀ Gʀᴏᴜᴘ Oʀ Jᴏɪɴ Oᴜʀ Gʀᴏᴜᴘ</b>", reply_markup=reply_markup)
