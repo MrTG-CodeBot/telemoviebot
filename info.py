8ef1c38bfa266881eec813a0a0cccff98d0e582d
obanaichu <153916736+obanaichu@users.noreply.github.com>
2023-12-18 19:12:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -45,7 +45,7 @@ SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(os.environ.get('API_ID', '8914119'))
 API_HASH = os.environ.get('API_HASH', '652bae601b07c928b811bdb310fdb4b0')
 BOT_TOKEN = os.environ.get('BOT_TOKEN', '6629383271:AAE1ZdxlW0ZMwbhGNXMdZpCvQZaW4LPDgX8')
-BARD_API_TOKEN = os.environ.get('cghU4f6_aFlWI7UHxm4sgWJnuuWkS2k7ROtLr_FGSHjGzMmITZ6S_DuJa4mIpUvRAv7o0g.')
+
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
